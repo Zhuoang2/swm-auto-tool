a157049bfef31c1f3a84a07abd103af774b46db6
Home Asus <doyijian@163.com>
2024-10-22 17:24:22 +0800

    fix: wrong import logic

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
-import sha
-import sha.cv as cv
-from sha.points import *
+from sha.cv import *
+import cv2
+from datetime import datetime
+
+
+def now():
+    return datetime.now().strftime('%H:%M:%S')
+
 
 if __name__ == '__main__':
-    # for image, name in [
-    #     (img_swm, 'swm'),
-    #     (img_box, 'box'),
-    #     (img_cola_b, 'cola_b'),
-    #     (img_cola_o, 'cola_o'),
-    #     (img_digua, 'digua'),
-    #     (img_shutiao, 'shutiao'),
-    # ]:
-    #     print(name)
-    #     res = match_many_object_on_image(img_full_screen, image, draw_rect=True, output_name=name + '.png')
-    #     print(f'there are {len(res)} {name} in the screen')
-    cv.fast_screen_shot(POS_GUEST_1_LT, POS_GUEST_1_RB)
+    for image in [
+        'test/swm.png',
+        'test/box.png',
+        'test/cola_b.png',
+        'test/cola_o.png',
+        'test/digua.png',
+        'test/shutiao.png',
+    ]:
+        full_screen = cv2.imread('test/full.png')
+        print(f'当前测试 {image}')
+        name = image.split('/')[-1].split('.')[0]
+        img = cv2.imread(image)
+        res = match_many_object_on_image(full_screen, img, draw_rect=True, save_file=False)
+        output_name = f'__test_{name}.png'
+        cv2.putText(full_screen, f'{name}: {len(res)} test at {now()}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imwrite(f'test/{output_name}', full_screen)
+        print(f'发现了 {len(res)} 个 {name}')
